7bfd488aea3ffeb8ea340d29828c11be612762a7
Akos Toth <akos.toth@cheppers.com>
2017-11-30 17:31:44 +0100

    now we are getting somewhere

--- bot.js
+++ bot.js
@@ -844,7 +844,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     roleID: bot.races[serverID].pingrole
                 }, function(err, response) {
                     logger.debug('addToRole: ' + response + ' / err: ' + err);
-                    logger.error('addToRole failed: ' + err.statusCode + ' '  + err.statusMessage + ' ' + Object.keys(err.response));
+                    logger.error('addToRole failed: ' + err.statusCode + ' '  + err.statusMessage + ' ' + err.response.code + ' ' + err.response.messae);
                 });
             break;
 
@@ -859,7 +859,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     roleID: bot.races[serverID].pingrole
                 }, function(err, response) {
                     logger.debug('removeFromRole: ' + response + ' / err: ' + err);
-                    logger.error('removeFromRole failed: ' + err.statusCode + ' '  + err.statusMessage + ' ' + Object.keys(err.response));
+                    logger.error('removeFromRole failed: ' + err.statusCode + ' '  + err.statusMessage + ' ' + err.response.code + ' ' + err.response.messae);
                 });
             break;
 
